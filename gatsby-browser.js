36ef670e8d4ccf2235c0a18d6ab0bfb110d7a6bd
Windows-Roy <roypers89@gmail.com>
2019-01-05 10:55:50 +0100

    fixade container width

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,6 @@ exports.onRouteUpdate = () => {
   // require('./src/styles/bootstrap.min.css')
 
   require('./src/styles/flexboxgrid.css')
-  //require('./src/styles/flexboxgrid.css')
 
   /* skulle ev. kunna lägga jquery koden här för att på varje onRouteUpdate
   köra koden. Alt. lägga den i layout för att på såvis få in den i varje layout.
